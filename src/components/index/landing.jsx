3c48a2c8ab03202d18400fe1db1240b53a926887
estebandvargasm <estebandvargasm@outlook.com>
2024-06-19 20:28:36 -0500

    Descripcion actividades

--- src/components/index/landing.jsx
+++ src/components/index/landing.jsx
@@ -1,12 +1,13 @@
 // landing prueba
 import React from 'react';
 import Header from './header/header';
+import Descripcion from './descripcion/descripcion';
 
 const Landing = () => {
     return (
         <>
         <Header /> 
-         
+        <Descripcion />
         </>
     );
     }
