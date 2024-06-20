9c8240751796d2f603296fbd958f7852c173189c
estebandvargasm <estebandvargasm@outlook.com>
2024-06-20 03:25:41 -0500

    Añadidas características

--- src/components/index/descripcion/descripcion.jsx
+++ src/components/index/descripcion/descripcion.jsx
@@ -6,30 +6,23 @@ const Descripcion = () => {
   return (
     <div>
 
-        <div className="calidad">
-
-            <div className="calidad_descripcion">
-                <Pencil size={40} />
-                <h2>Calidad</h2>
-                <p>Nuestro sistema de gestión de la calidad está certificado según la ISO 9001:2015 por Bureau Veritas Certification.</p>
-            </div>
-            <div className="calidad_imagenes">
-                <img class="imagen" src="imagenes/Logo-Mind.png" alt="Mind"/>
-                <img class="imagen" src="imagenes/Certificacion-Veritas.jpeg" alt="Certificación BUREAU VERITAS"/>
-            </div>
-            
-        </div>
-
         <div className="descripcion">
 
-            <div className="section">
-                <Gear strokeWidth={2} size={40} />
+            <div className="caracteristica">
+                <Gear strokeWidth={2} size={40} color={"#006278"}/>
                 <h2>Fabricación</h2>
                 <p>Contamos con la capacidad para conformar el metal a los requerimientos de su diseño. Conozca nuestra capacidad productiva.</p>
             </div>
 
-            <div className="section">
-                <StatisticUp strokeWidth={2} size={40} />
+            <div className="caracteristica">
+                <Pencil strokeWidth={2} size={40} color={"#006278"}/>
+                <h2>Calidad</h2>
+                <p>Nuestro sistema de gestión de la calidad está certificado según la ISO 9001:2015 por Bureau Veritas Certification.</p>
+                <img class="imagen1" src="/images/certificacion-veritas.png" alt="Certificación BUREAU VERITAS"/>
+            </div>
+
+            <div className="caracteristica">
+                <StatisticUp strokeWidth={2} size={40} color={"#006278"}/>
                 <h2>Diseño</h2>
                 <p>Diseñamos y desarrollamos soluciones a partir de sus necesidades. Conozca como podemos desarrollar su idea.</p>
             </div>
