db7362abe461c6ea38ca73b2c8d8206964cab046
estebandvargasm <estebandvargasm@outlook.com>
2024-07-05 05:24:25 -0500

    Se cambió Nosotros

--- src/components/sobreNosotros/nosotros.jsx
+++ src/components/sobreNosotros/nosotros.jsx
@@ -3,48 +3,45 @@ import "./SobreNosotros.css";
 import Navbar from "../navbar/navbar";
 import Footer from "../index/footer/footer";
 import BotonWhatsapp from "../index/boton-whatsapp/boton-whatsapp.jsx";
-import { FaFileDownload } from "react-icons/fa"; // Importar íconos
+import { FaFileDownload } from "react-icons/fa";
 
 const SobreNosotros = () => {
   return (
     <>
       <Navbar />
       <section className="sobre-nosotros">
-        <div className="sobre-nosotros-container">
-          <div className="sobre-nosotros-text">
-            <h1>Sobre Nosotros</h1>
-            <p>
-              Mind SAS BIC es una empresa del sector metalmecánico, con más de
-              30 años en la industria, que trabaja transformando el metal para
-              convertirlo en piezas de distintas características que suplen
-              necesidades desde el sector petrolero hasta empresas de decoración
-              y acabados.
-            </p>
-            <h2>Somos una empresa BIC</h2>
-            <p>
-              A lo largo de la historia Mind SAS BIC, ha buscado evolucionar,
-              siempre con un enfoque humanista que le permite impactar
-              positivamente tanto el entorno al que pertenece como el equipo con
-              el que cuenta. Al conocer las características de una empresa BIC,
-              las acciones que se realizan al interior de la organización
-              muestran una oportunidad para orientar claramente algunos de los
-              esfuerzos y aportar en temas de medio ambiente, de bienestar para
-              nuestras partes interesadas y en general, de participar en el
-              mercado de una manera más positiva.
-            </p>
-            <p>
-              Es así como en el año 2022 adoptamos la condición BIC y a
-              continuación presentamos el primer reporte de Gestión BIC.
-            </p>
-          </div>
-          <div className="sobre-nosotros-image">
-            <img src="/images/SobreNosotros.jpeg" alt="Nuestra Empresa" />
-          </div>
+        <div className="sobre-nosotros-header">
+          <h1>Sobre Nosotros</h1>
         </div>
+      </section>
+      <section className="sobre-nosotros-info">
+        <p className="">
+          Mind SAS BIC es una empresa del sector metalmecánico, con más de
+          30 años en la industria, que trabaja transformando el metal para
+          convertirlo en piezas de distintas características que suplen
+          necesidades desde el sector petrolero hasta empresas de decoración
+          y acabados.
+        </p>
+        <h2>Somos una empresa BIC</h2>
+        <p>
+          A lo largo de la historia Mind SAS BIC, ha buscado evolucionar,
+          siempre con un enfoque humanista que le permite impactar
+          positivamente tanto el entorno al que pertenece como el equipo con
+          el que cuenta. Al conocer las características de una empresa BIC,
+          las acciones que se realizan al interior de la organización
+          muestran una oportunidad para orientar claramente algunos de los
+          esfuerzos y aportar en temas de medio ambiente, de bienestar para
+          nuestras partes interesadas y en general, de participar en el
+          mercado de una manera más positiva.
+        </p>
+        <p>
+          Es así como en el año 2022 adoptamos la condición BIC y a
+          continuación presentamos el primer reporte de Gestión BIC.
+        </p>
       </section>
       <section className="reporte-bic">
         <img src="/images/bicAzul.png" alt="Logo BIC" />{" "}
-        <h2>REPORTE BIC</h2>
+        <h2>Reporte BIC</h2>
         <p>
           Para conocer más acerca de nuestro compromiso como Sociedad BIC, lo
           invitamos a leer nuestro primer Reporte BIC. Haga clic para descargar
