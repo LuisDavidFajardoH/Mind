a1711dd53bfa77215f2a878d80583014c96a32a1
DvidFH <luisda038outlook.com>
2024-07-18 09:21:09 -0500

    contacto

--- src/components/contactenos/contacto.jsx
+++ src/components/contactenos/contacto.jsx
@@ -21,14 +21,14 @@ const Contact = () => {
       <HeaderPagina titulo="Contacto" imagenFondo="contactenos.jpg" />
       <Container className="main-container">
         <Box className="left-section">
-          <Typography variant="h5" className="venta-title">Contactanos</Typography>
+          <Typography variant="h5" className="venta-title">Contáctanos</Typography>
           <Typography variant="h4" className="project-title">MIND SAS</Typography>
           <Typography variant="body1" className="description">
-          En MIND SAS, con más de 25 años de experiencia en el sector metalmecánico colombiano, diseñamos y desarrollamos soluciones a partir de sus necesidades. Con una capacidad productiva avanzada y un sistema de gestión de calidad certificado según la ISO 9001:2015 por Bureau Veritas Certification, estamos listos para transformar sus ideas en realidad. ¡Visítenos en Av. Américas Calle 20 # 39 – 35, Bogotá D.C. o agende su cita hoy mismo al (+57) 311 289 7588!
+            En MIND SAS, con más de 25 años de experiencia en el sector metalmecánico colombiano, diseñamos y desarrollamos soluciones a partir de sus necesidades. Con una capacidad productiva avanzada y un sistema de gestión de calidad certificado según la ISO 9001:2015 por Bureau Veritas Certification, estamos listos para transformar sus ideas en realidad. ¡Visítenos en Av. Américas Calle 20 # 39 – 35, Bogotá D.C. o agende su cita hoy mismo al (+57) 311 289 7588!
           </Typography>
           <Box className="location-box">
             <LocationOnIcon />
-            <a href="https://maps.app.goo.gl/HvCYpJ1PQymHbyez6" target="_blank" class="location-text">Ver ubicación</a>
+            <a href="https://maps.app.goo.gl/HvCYpJ1PQymHbyez6" target="_blank" className="location-text">Ver ubicación</a>
           </Box>
           <Card className="social-card">
             <CardContent>
@@ -54,27 +54,27 @@ const Contact = () => {
       <Container className="info-container">
         <Card className="info-card" style={{ backgroundImage: 'url(images/fondoContacto.webp)' }}>
           <CardContent className="info-overlay">
-              <CardContent className="info-content">
-                <Box className="info-section">
-                  <Typography variant="h6" className="info-title">HORARIO</Typography>
-                  <Typography variant="body2">Lunes a Viernes: 8:00 am a 5:00 pm</Typography>
-                  <Typography variant="body2">Sábados: Cerrado</Typography>
-                  <Typography variant="body2">Domingos: Cerrado</Typography>
-                </Box>
-                <Box className="info-section">
-                  <Typography variant="h6" className="info-title">E-MAIL</Typography>
-                  <Typography variant="body2"> info@metalmindsa.com</Typography>
-                </Box>
-                <Box className="info-section">
-                  <Typography variant="h6" className="info-title">TELÉFONO</Typography>
-                  <Typography variant="body2"> (+57) 311 289 7588</Typography>
-                </Box>
-                <Box className="info-section">
-                  <Typography variant="h6" className="info-title">DIRECCIÓN</Typography>
-                  <Typography variant="body2">Av. Américas Calle 20 # 39 – 35</Typography>
-                  <Typography variant="body2">Bogotá D.C. – Colombia</Typography>
-                </Box>
-              </CardContent>
+            <CardContent className="info-content">
+              <Box className="info-section">
+                <Typography variant="h6" className="info-title">HORARIO</Typography>
+                <Typography variant="body2">Lunes a Viernes: 8:00 am a 5:00 pm</Typography>
+                <Typography variant="body2">Sábados: Cerrado</Typography>
+                <Typography variant="body2">Domingos: Cerrado</Typography>
+              </Box>
+              <Box className="info-section">
+                <Typography variant="h6" className="info-title">E-MAIL</Typography>
+                <Typography variant="body2"> info@metalmindsa.com</Typography>
+              </Box>
+              <Box className="info-section">
+                <Typography variant="h6" className="info-title">TELÉFONO</Typography>
+                <Typography variant="body2"> (+57) 311 289 7588</Typography>
+              </Box>
+              <Box className="info-section">
+                <Typography variant="h6" className="info-title">DIRECCIÓN</Typography>
+                <Typography variant="body2">Av. Américas Calle 20 # 39 – 35</Typography>
+                <Typography variant="body2">Bogotá D.C. – Colombia</Typography>
+              </Box>
+            </CardContent>
           </CardContent>
         </Card>
       </Container>
